27aecca0e92edeb9e00937c9fd0ea5f2113c77da
plusjade <plusjade@gmail.com>
2010-10-15 17:00:40 -0700

    new rails 3 project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jdlightings::Application.config.secret_token = '8891cbf4ae774ebadc7147d3cb49413defcf32884fa78922a2f0fd44e26491c5e52f49edbac36ae8eb21d1bfcd0db5779de8a69f7859b243307487913b559172'
