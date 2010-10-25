4c142045dd4f34803fd6a2471a57c2db390fa8c0
plusjade <plusjade@gmail.com>
2010-10-24 21:06:48 -0700

    to sammy!

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,28 +1,35 @@
 class PagesController < ApplicationController
   
-  
+  layout proc { |c| c.request.xhr? ? false : "application" }
+  before_filter :setup
+      
   def index
     
   end 
   
   
   def about
+    @parent_nav = "company"
+
     @categories = Category.all
-    
     @products = Product.all
     
   end
 
   def conditions
+    @parent_nav = "company"
   end 
 
   def dealer_inquiry
+    @parent_nav = "company"
   end
 
   def contact
+    @parent_nav = "contact"
   end
   
   def support
+    @parent_nav = "media"
   end
   
   def faq
@@ -34,5 +41,16 @@ class PagesController < ApplicationController
   def privacy_and_terms
   end
   
+  def news
+    @parent_nav = "media"
+  end 
+  
+  
+  private
+  
+    def setup
+      @parent_nav = 'nothing'
+      @page = request.fullpath.split('/').pop
+    end
   
 end
